f1aead811ac552747eee13de8ba90272e2ab4567
Renzo Diaz <renzodd2001@gmail.com>
2021-09-23 20:01:51 -0500

    Added Source

---
+++ src/Epidemic Simulator/Clases.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Epidemic_Simulator
+{
+    public enum Estado { Sano, Enfermo, Curado, Muerto};
+    public static class StaticRandom
+    {
+        private static int seed;
+
+        private static ThreadLocal<Random> threadLocal = new ThreadLocal<Random>
+            (() => new Random(Interlocked.Increment(ref seed)));
+
+        static StaticRandom()
+        {
+            seed = Environment.TickCount;
+        }
+
+        public static Random Instance { get { return threadLocal.Value; } }
+    }
+
+    public class Coordenadas
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public Coordenadas(Graphics g)
+        {
+            Random j = new Random();
+            x = StaticRandom.Instance.Next(10, (int)g.VisibleClipBounds.Right - 10);
+            y = StaticRandom.Instance.Next(10, (int)g.VisibleClipBounds.Bottom - 10);
+        }
+        public override bool Equals(object obj)
+        {
+            float sensibilidad = 10.0f;
+            return Math.Abs(x - ((Coordenadas)obj).x) <= sensibilidad && Math.Abs(y - ((Coordenadas)obj).y) <= sensibilidad;
+        }
+        public bool Near(object obj, float sensibilidad)
+        {
+            return Math.Abs(x - ((Coordenadas)obj).x) <= sensibilidad && Math.Abs(y - ((Coordenadas)obj).y) <= sensibilidad;
+        }
+    }
+    public class Hogar
+    {
+        public Coordenadas cord { get; set; }
+        public int ancho { get; set; }
+        public int alto { get; set; }
+        double n;
+        public Hogar(Graphics g, int n)
+        {
+            
+            this.n = n;
+            Resize(g);
+        }
+        public void Resize(Graphics g)
+        {
+            if (n < 10)
+            {
+                cord = new Coordenadas(g);
+                cord.x = (int)((10* n + 1) * (g.VisibleClipBounds.Right) / 100);
+                cord.y = (int)((1) * (g.VisibleClipBounds.Bottom) / 100);
+
+                ancho = (int)((8) * (g.VisibleClipBounds.Right) / 100);
+                alto = (int)((8) * (g.VisibleClipBounds.Bottom) / 100);
+
+            }
+            else if (n < 26)
+            {
+                if (n % 2 == 0)
+                {
+                    cord = new Coordenadas(g);
+                    cord.x = (int)((1) * (g.VisibleClipBounds.Right) / 100);
+                    cord.y = (int)((((n / 2) - 4) * 10) * (g.VisibleClipBounds.Bottom) / 100);
+
+                    ancho = (int)((8) * (g.VisibleClipBounds.Right) / 100);
+                    alto = (int)((8) * (g.VisibleClipBounds.Bottom) / 100);
+                }
+                else
+                {
+                    cord = new Coordenadas(g);
+                    cord.x = (int)((91) * (g.VisibleClipBounds.Right) / 100);
+                    cord.y = (int)(((((n-1) / 2) - 4) * 10) * (g.VisibleClipBounds.Bottom) / 100);
+
+                    ancho = (int)((8) * (g.VisibleClipBounds.Right) / 100);
+                    alto = (int)((8) * (g.VisibleClipBounds.Bottom) / 100);
+                }
+            }
+            else
+            {
+                cord = new Coordenadas(g);
+                cord.x = (int)((10 * (n - 26) + 1) * (g.VisibleClipBounds.Right) / 100);
+                cord.y = (int)((91) * (g.VisibleClipBounds.Bottom) / 100);
+
+                ancho = (int)((8) * (g.VisibleClipBounds.Right) / 100);
+                alto = (int)((8) * (g.VisibleClipBounds.Bottom) / 100);
+            }
+
+
+
+
+        }
+        public void Dibujar(Graphics g)
+        {
+           if (cord != null)
+            g.DrawRectangle(Pens.Lime, cord.x, cord.y, ancho, alto);
+        }
+    }
+    public class Individuo
+    {
+        public Coordenadas posicion { get; set; }
+        public Coordenadas destino { get; set; }
+        public Hogar hogar { get; set; }
+        public Estado estado { get; set; }
+        public bool asintomatico { get; set; }
+        public bool Higiene { get; set; }
+        public bool cuarentena { get; set; }
+        public bool DistanciamientoSocial { get; set; }
+
+        public int dias_enfermo { get; set; }
+
+        int T = 5; //Tamaño
+        int V = 9; //Velocidad
+        float dx;
+        float dy;
+        public Individuo(Graphics g)
+        {
+
+            estado = (Estado.Sano);// (StaticRandom.Instance.Next(0, 4));
+            posicion = destino = new Coordenadas(g);
+        }
+        public void Mover(Graphics g, int cuarentena, int distanciamiento)
+        {
+            if (estado != Estado.Muerto)
+            {
+                if (posicion.Equals(destino))
+                {
+                    destino = new Coordenadas(g);
+
+                    if (cuarentena != 0)
+                        if (StaticRandom.Instance.Next(0, 99) < cuarentena)
+                        {
+                            destino.x = StaticRandom.Instance.Next((int)hogar.cord.x, (int)hogar.cord.x + hogar.ancho);
+                            destino.y = StaticRandom.Instance.Next((int)hogar.cord.y, (int)hogar.cord.y + hogar.alto);
+
+                        }
+
+
+                    int simbX = 0;
+                    int simbY = 0;
+
+                    if (destino.x < posicion.x)
+                    { dx = posicion.x - destino.x; simbX = -1; }
+                    else if (destino.x > posicion.x)
+                    { dx = destino.x - posicion.x; simbX = 1; }
+                    else
+                        dx = 0;
+
+                    if (destino.y < posicion.y)
+                    { dy = posicion.y - destino.y; simbY = -1; }
+                    else if (destino.y > posicion.y)
+                    { dy = destino.y - posicion.y; simbY = 1; }
+                    else
+                        dy = 0;
+
+                    double ang = Math.Atan2(dy, dx);
+
+                    dy = (float)(V * simbY * Math.Sin(ang));
+                    dx = (float)(V * simbX * Math.Cos(ang));
+                }
+                posicion.x += dx;
+                posicion.y += dy;
+            }
+        }
+        public void Dibujar(Graphics g, int i, bool ver_proximidad)
+        {
+            //g.FillEllipse(Brushes.Red, destino.x - (T / 2), destino.y - (T / 2), T, T);
+
+            if (estado != Estado.Muerto)
+            {
+                if (estado == Estado.Enfermo)
+                    g.FillEllipse(Brushes.Firebrick, posicion.x - (T / 2), posicion.y - (T / 2), T, T);
+                else if(estado == Estado.Curado)
+                    g.FillEllipse(Brushes.DarkOliveGreen, posicion.x - (T / 2), posicion.y - (T / 2), T, T);
+                else
+                    g.FillEllipse(new SolidBrush(Color.FromArgb(0, 120, 215)), posicion.x - (T / 2), posicion.y - (T / 2), T, T);
+
+                if (ver_proximidad)
+                    g.DrawRectangle(Pens.Red, posicion.x - i, posicion.y - i, 2 * i, 2 * i);
+
+                if (asintomatico)
+                    g.DrawEllipse(Pens.White, posicion.x - (T / 2), posicion.y - (T / 2), T, T);
+
+            }
+            else
+                g.FillEllipse(Brushes.Black, posicion.x - (T / 2), posicion.y - (T / 2), T, T);
+
+            //g.DrawString(i.ToString(), new Font("Arial", 10), Brushes.White, posicion.x - (T / 2), posicion.y - (T / 2));
+        }
+    }
+}
---
+++ src/Epidemic Simulator/Principal.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Epidemic_Simulator
+{
+    public partial class Principal : Form
+    {
+        List<Individuo> individuos = new List<Individuo>();
+        List<Hogar> hogares = new List<Hogar>();
+
+
+        Graphics g;
+        Graphics H;
+        public Principal()
+        {
+            InitializeComponent();
+            g = scene.CreateGraphics();
+            H = scene.CreateGraphics();
+
+        }
+
+        void EmpezarSimulacion()
+        {
+            Graphics g = scene.CreateGraphics();
+
+            individuos.Clear();
+            for (int i = 0; i < 36; i++)
+                hogares.Add(new Hogar(g, i));
+
+            individuos.Clear();
+            for (int j = 0; j < nudPopulation.Value; j++)
+            {
+                Individuo i = new Individuo(g);
+                if (cbDistanciamientoSocial.Checked == true)
+                {
+                    if (StaticRandom.Instance.Next(0, 100) < nudDistanciamiento.Value)
+                        i.DistanciamientoSocial = true;
+                }
+                if (cbCuarentena.Checked == true)
+                {
+                    if (StaticRandom.Instance.Next(0, 100) < nudCuarentena.Value)
+                        i.cuarentena = true;
+                }
+                if (cbHigiene.Checked == true)
+                {
+                    if (StaticRandom.Instance.Next(0, 100) < nudHigiene.Value)
+                        i.Higiene = true;
+                }
+                if (cbAsintomatico.Checked == true)
+                {
+                    if (StaticRandom.Instance.Next(0, 100) < nudAsintomatico.Value)
+                        i.asintomatico = true;
+                }
+
+                i.hogar = hogares[StaticRandom.Instance.Next(0, 36)];
+
+                individuos.Add(i);
+            }
+            for (int j = 0; j < nudInfectedPopulation.Value; j++)
+                individuos[j].estado = Estado.Enfermo;
+
+
+           
+        }
+        ulong time = 0;
+        void MostrarHora()
+        {
+            lblTime.Text = time.ToString();
+               //years.ToString() + " Years, " +
+               //months.ToString() + " Months, " +
+               //days.ToString() + " Days, " +
+               //hours.ToString() + " Hours, " +
+               //minutes.ToString() + " Minutes";
+        }
+
+        int sano = 0, enfermo = 0, muerto = 0, curado = 0, poblacion = 0;
+        int SANO = 0, ENFERMO = 1, MUERTO = 0, CURADO = 0, POBLACION = 0;
+        void Proceso()
+        {
+            while (true)
+            {
+                try
+                {
+                    sano = 0; enfermo = 0; muerto = 0; curado = 0; poblacion = 0;
+                    time++;
+                    foreach (Individuo i in individuos)
+                    {
+                        i.Mover(H, (int)nudCuarentena.Value, (int)nudDistanciamiento.Value);
+
+                        foreach (Individuo j in individuos)
+                        {
+                            if (j != null)
+                                if (j.posicion.Near(i.posicion, (float)nudProximidad.Value))
+                                {
+                                    if (j.estado == Estado.Enfermo && i.estado == Estado.Sano)
+                                    {
+                                        if (i.Higiene == false)
+                                        {
+                                            if (StaticRandom.Instance.Next(0, 100) < nudTasaContagio.Value)
+                                                i.estado = Estado.Enfermo;
+                                        }
+                                        else
+                                        {
+                                            if (StaticRandom.Instance.Next(0, 100) < nudTasaContagio.Value && StaticRandom.Instance.Next(0, 100) % 10 == 0)
+                                                i.estado = Estado.Enfermo;
+                                        }
+                                    }
+
+                                }
+                        }
+
+                        if (i.estado == Estado.Enfermo)
+                            i.dias_enfermo++;
+
+                        if (i.dias_enfermo >= nudDiasEnfermo.Value)
+                        {
+                            if (i.asintomatico == false)
+                            {
+                                if (StaticRandom.Instance.Next(0, 100) < nudTasaMortalidad.Value)
+                                    i.estado = Estado.Muerto;
+                                else
+                                    i.estado = Estado.Curado;
+                            }
+                            else
+                                i.estado = Estado.Curado;
+                            
+                            i.dias_enfermo = 0;
+                        }
+
+                        if (i.estado == Estado.Sano)
+                            sano++;
+                        else if (i.estado == Estado.Enfermo)
+                            enfermo++;
+                        else if (i.estado == Estado.Muerto)
+                            muerto++;
+                        else if (i.estado == Estado.Curado)
+                            curado++;
+                    }
+
+                    //Grafica general
+                    Graphics G = grafica.CreateGraphics();
+                    poblacion = sano + enfermo + muerto + curado;
+
+
+                    this.SANO = sano;
+                    CURADO = curado;
+                    ENFERMO = enfermo;
+                    MUERTO = muerto;
+                    POBLACION = poblacion;
+
+                    float SANO = G.VisibleClipBounds.Bottom * (sano / (float)poblacion);
+                    float INFE = G.VisibleClipBounds.Bottom * (enfermo / (float)poblacion);
+                    float MUER = G.VisibleClipBounds.Bottom * (muerto / (float)poblacion);
+                    float CURA = G.VisibleClipBounds.Bottom * (curado / (float)poblacion);
+
+                    G.DrawLine(new Pen(Color.FromArgb(0, 120, 215)), time % G.VisibleClipBounds.Right, 0, time % G.VisibleClipBounds.Right, SANO);
+                    G.DrawLine(new Pen(Brushes.Firebrick), time % G.VisibleClipBounds.Right, SANO, time % G.VisibleClipBounds.Right, SANO + INFE);
+                    G.DrawLine(new Pen(Brushes.Black), time % G.VisibleClipBounds.Right, SANO + INFE, time % G.VisibleClipBounds.Right, SANO + INFE + MUER);
+                    G.DrawLine(new Pen(Brushes.DarkOliveGreen), time % G.VisibleClipBounds.Right, SANO + INFE + MUER, time % G.VisibleClipBounds.Right, SANO + INFE + MUER + CURA);
+
+                    //Grafica de enfermos
+                    G = enfermos.CreateGraphics();
+                    G.DrawLine(new Pen(Color.FromArgb(28, 0, 28)), time % G.VisibleClipBounds.Right, 0, time % G.VisibleClipBounds.Right, G.VisibleClipBounds.Bottom);
+                    G.DrawLine(new Pen(Brushes.Firebrick), time % G.VisibleClipBounds.Right, G.VisibleClipBounds.Bottom, time % G.VisibleClipBounds.Right, G.VisibleClipBounds.Bottom - G.VisibleClipBounds.Bottom * (enfermo / (float)poblacion));
+
+
+
+                    //if (enfermo == 0)
+                    //    break;
+
+                    if (proceso == false)
+                    { 
+                        proceso = true; 
+                        break; 
+                    }
+
+                    if (kill == true)
+                    {  
+                        kill = false; 
+                        break; 
+                    }
+                }
+                catch { }
+            }
+        }
+
+        void GraficarMapa()
+        {
+            while (true)
+            {
+                try
+                {
+                    BufferedGraphicsContext espacioBuffer = BufferedGraphicsManager.Current;
+                    BufferedGraphics buffer = espacioBuffer.Allocate(g, scene.ClientRectangle);
+                    buffer.Graphics.Clear(Color.FromArgb(50, 50, 50));
+                    foreach (Individuo i in individuos)
+                        if (i != null)
+                            i.Dibujar(buffer.Graphics, (int)nudProximidad.Value, cbViewProximidad.Checked);
+
+                    if (cbCuarentena.Checked)
+                        foreach (Hogar h in hogares)
+                            if (h != null)
+                                h.Dibujar(buffer.Graphics);
+                    buffer.Render(g);
+                }
+                catch { }
+            }
+        }
+
+        private void cbDistanciamientoSocial_CheckedChanged(object sender, EventArgs e)
+        {
+            nudDistanciamiento.Enabled = !nudDistanciamiento.Enabled;
+            if (nudDistanciamiento.Enabled)
+                nudDistanciamiento.Value = 60;
+            else
+                nudDistanciamiento.Value = 0;
+        }
+
+        private void cbCuarentena_CheckedChanged(object sender, EventArgs e)
+        {
+            nudCuarentena.Enabled = !nudCuarentena.Enabled;
+            if (nudCuarentena.Enabled)
+                nudCuarentena.Value = 100;
+            else
+                nudCuarentena.Value = 0;
+        }
+
+        private void cbHigiene_CheckedChanged(object sender, EventArgs e)
+        {
+            nudHigiene.Enabled = !nudHigiene.Enabled;
+        }
+
+        private void cbAsintomatico_CheckedChanged(object sender, EventArgs e)
+        {
+            nudAsintomatico.Enabled = !nudAsintomatico.Enabled;
+        }
+
+        private void nudPopulation_ValueChanged(object sender, EventArgs e)
+        {
+            nudInfectedPopulation.Maximum = nudPopulation.Value;
+            lblSanos.Text = nudPopulation.Value.ToString();
+        }
+
+        private void nudInfectedPopulation_ValueChanged(object sender, EventArgs e)
+        {
+            lblInfectados.Text = nudInfectedPopulation.Value.ToString();
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        int time_graph = 0;
+
+        Task T;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UCCPOBzJTlf1u-UMRGMyDXVQ");
+        }
+
+        private void scene_Resize(object sender, EventArgs e)
+        {
+            Graphics g = scene.CreateGraphics();
+            foreach (Hogar h in hogares)
+                h.Resize(g);
+        }
+
+        private void clock_Tick(object sender, EventArgs e)
+        {
+            MostrarHora();
+            g = scene.CreateGraphics();
+            H = scene.CreateGraphics();
+            time_graph++;
+
+
+            lblCurados.Text = CURADO.ToString();
+            lblInfectados.Text = ENFERMO.ToString();
+            lblMuertos.Text = MUERTO.ToString();
+            lblSanos.Text = SANO.ToString();
+        }
+        #region Herramientas
+        
+        bool kill = false;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (lblInfectados.Text == "0")
+                btnRestart_Click(null, null);
+            proceso = true;
+            clock.Enabled = true;
+
+            T = new Task(Proceso);
+            T.Start();
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+        }    
+            
+
+        
+        bool proceso;
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            proceso = false;
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+        }
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            EmpezarSimulacion();
+            //clock_Tick(null, null);
+            time = 0; time_graph = 0;
+            MostrarHora();
+
+
+            grafica.CreateGraphics().Clear(Color.FromArgb(28, 28, 28));
+            enfermos.CreateGraphics().Clear(Color.FromArgb(28, 28, 28));
+            scene.CreateGraphics().Clear(Color.FromArgb(50, 50, 50));
+        }
+       
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            EmpezarSimulacion();
+            MostrarHora();
+
+            Task T1 = new Task(GraficarMapa);
+            T1.Start();
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void scene_DoubleClick(object sender, EventArgs e)
+        {
+            clock.Enabled = !clock.Enabled;
+
+        }
+        #endregion
+    }
+}
